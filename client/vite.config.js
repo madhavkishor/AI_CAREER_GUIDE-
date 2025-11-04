6022e1d552abaf724f15e8185987616c69a16b66
MADHAV KISHOR <145675322+madhavkishor@users.noreply.github.com>
2025-11-04 12:02:59 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 👈 ensures Vercel finds built files
+  },
+  server: {
+    port: 5173, // optional (for local dev)
+  },
 })
+
